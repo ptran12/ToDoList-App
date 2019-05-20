4dcea96ca94f6b82fdbc3620fe12af7089bf71c2
Phi Tran <phitran.722@gmail.com>
2019-05-19 23:10:34 -0500

    ESJ template now renders day on topage

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -9,16 +10,38 @@ app.set('view engine', 'ejs');
 app.get("/", function(req, res){
 
     var today = new Date();
-    var currentDay = today.getDate();
+    var currentDay = today.getDay();
     var day = "";
 
-    if (currentDay === 6 || currentDay === 0) {
-        day = "Weekend";
-    } else {
-        day = "Weekday";
+    switch (currentDay) {
+      case 0:
+        day = "Sunday";
+        break;
+      case 1:
+        day = "Monday";
+        break;
+      case 2:
+        day = "Tuesday";
+        break;
+      case 3:
+        day = "Wednesday";
+        break;
+      case 4:
+        day = "Thursday";
+        break;
+      case 5:
+        day = "Friday";
+        break;
+      case 6:
+        day = "Saturday";
+        break;
+        default:
+        console.log("Error: " + currentDay);
     }
 
-    res.render("list", {kindOfDay: day});
+    res.render("list", { 
+        kindOfDay: day
+    });
 
 });
 
